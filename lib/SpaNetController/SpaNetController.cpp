b191001277a1d6c235b40fe6a528efb4cb92a5aa
Wayne Love <wayne@gsjs.org>
2021-11-06 08:01:57 +1100

    Reword serial string parsing

--- lib/SpaNetController/SpaNetController.cpp
+++ lib/SpaNetController/SpaNetController.cpp
@@ -1,6 +1,51 @@
 #include "SpaNetController.h"
 
 
+
+
+Register::Register(int req) {
+  requiredFields = req;
+}
+
+
+bool Register::updateRegister(const char update[]) {
+
+  strcpy(reg, update);
+  int len=strlen(reg);
+  int y=1;
+  element[0]=&reg[0];
+  for(int x=0; x<len; x++) {        //split the string into a number of smaller strings
+    if (reg[x]==',') {              //each terminated by 0x0. create a pointer to the 
+      reg[x]=0;                     //start of each smaller string.
+      element[y]=&reg[x+1];
+      y++;
+      }
+  }
+
+  if (y==requiredFields) {
+    valid = true;
+
+  } else {
+    debugW("Error parsing register, expected %d fields, got %d",requiredFields,y);
+    debugD("Update register request - %s",update);
+    valid = false;
+  }
+
+  return valid;
+}
+
+char* Register::getField(int field) {
+  return element[field];
+}
+
+bool Register::isValid() {
+  return valid;
+}
+
+
+
+// *** Start SpaNetController
+
 SpaNetController::SpaNetController() {
     Serial2.begin(38400,SERIAL_8N1, 16, 17);
     Serial2.setTimeout(500); 
@@ -25,32 +70,48 @@ float SpaNetController::getHpumpAmbTemp(){
 
 bool SpaNetController::parseStatus(String str) {
 
-  int element=1;
-  int commaIndex=str.indexOf(',');
-  int elementBoundaries[290];
+//R4 is hit and miss as to if it returns all its data.
+//So need to work on a variable number of rows 
+//returned.
 
-  elementBoundaries[0]=0;
-  
-  while (commaIndex>-1){
-    elementBoundaries[element] = commaIndex;
-    element++;
-    commaIndex=str.indexOf(',',commaIndex+1);
+  int currentPos = 0;
+  int colonIndex = str.indexOf(':');
+
+  while (colonIndex>-1) {
+    int r = str.indexOf('R',currentPos);
+    if (r>-1){
+      int currentReg = -1;
+      char rChar = str.charAt(r+1); //should really check if r is the last character of string
+      if (rChar=='F') { currentReg = 0; }
+      else if (rChar=='2') { currentReg = 1; }
+      else if (rChar=='3') { currentReg = 2; }
+      else if (rChar=='4') { currentReg = 3; }
+      else if (rChar=='5') { currentReg = 4; }
+      else if (rChar=='6') { currentReg = 5; }
+      else if (rChar=='7') { currentReg = 6; }
+      else if (rChar=='9') { currentReg = 7; }
+      else if (rChar=='A') { currentReg = 8; }
+      else if (rChar=='B') { currentReg = 9; }
+      else if (rChar=='C') { currentReg = 10; }
+      else if (rChar=='E') { currentReg = 11; }
+      else if (rChar=='G') { currentReg = 12; }
+      if (currentReg>-1) {
+        !registers[currentReg].updateRegister(str.substring(currentPos,colonIndex).c_str());
+      }
+    }
+    currentPos=colonIndex+1;
+    colonIndex=str.indexOf(':',currentPos);
   }
 
-  elementBoundaries[element] = str.length();
-
-  if (element != 290) { 
-    debugW("Wrong number of parameters read, read %d, expecting 289", element-1);
-    return false;
-  }
-  else {
-    debugI("Successful read of SpaNet status");
-    amps = float(str.substring(elementBoundaries[2]+1,elementBoundaries[3]).toInt())/10;
-    volts = str.substring(elementBoundaries[3]+1,elementBoundaries[4]).toInt();
-    hpump_amb_temperature = float(str.substring(elementBoundaries[251]+1,elementBoundaries[252]).toInt());
-    lightsOn = bool(str.substring(elementBoundaries[106]+1,elementBoundaries[107]).toInt());
+  // First datapoint in registers is at array position 2
+  if (registers[1].isValid() && registers[5].isValid() && registers[11].isValid()) {
+    amps = float(String(registers[1].getField(2)).toInt())/10;
+    volts = String(registers[1].getField(3)).toInt();
+    hpump_amb_temperature = String(registers[11].getField(11)).toInt();
+    lightsOn = String(registers[4].getField(15)).toInt();
     return true;
-  }
+  } else { return false; }
+
 }
 
 String SpaNetController::sendCommand(String cmd) {
--- lib/SpaNetController/SpaNetController.h
+++ lib/SpaNetController/SpaNetController.h
@@ -11,14 +11,34 @@
 
 extern RemoteDebug Debug;
 
+class Register {
+    public:
+        Register(int req);
+
+
+        bool updateRegister(const char update[]);
+        bool isValid();
+        char* getField(int field);
+
+    private:
+        int requiredFields;
+        char reg[256];
+        char* element[64];
+        bool valid=false;
+
+
+};
+
 class SpaNetController {
     private:
-        float amps;
+
+        float amps; 
         int volts;
-        
         float hpump_amb_temperature;
 
         bool lightsOn;
+
+        Register registers[13]={1,33,30,31,29,31,34,15,15,15,17,33,18};
 
         ulong _nextUpdate=millis();
         ulong lastCommand = millis();
@@ -44,8 +64,8 @@ class SpaNetController {
         float getAmps();
         int getVolts();
         float getHpumpAmbTemp();
+
         bool isLightsOn();
-        
         bool setLightsOn(bool flag);
         
         SpaNetController();
