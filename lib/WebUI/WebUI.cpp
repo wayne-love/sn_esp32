aace5c45d0a2c9fd5726346eb657d6565386e315
Jonathan Giles <jonathan@jonathangiles.net>
2022-12-23 20:17:47 +1300

    Initial commit to introduce ESP8266 build support, and a GitHub action to keep us honest.

--- lib/WebUI/WebUI.cpp
+++ lib/WebUI/WebUI.cpp
@@ -1,12 +1,20 @@
 #include "WebUI.h"
 
+#if defined(ESP8266)
+    #define UPDATE_SIZE_UNKNOWN 0XFFFFFFFF
+#endif
+
 WebUI::WebUI(SpaNetController *spa) {
     _spa = spa;
 }
 
 void WebUI::begin() {
         
-    server.reset(new WebServer(80));
+    #if defined(ESP8266)
+        server.reset(new ESP8266WebServer(80));
+    #elif defined(ESP32)
+        server.reset(new WebServer(80));
+    #endif
 
     server->on("/", HTTP_GET, [&]() {
         server->sendHeader("Connection", "close");
@@ -29,18 +37,18 @@ void WebUI::begin() {
         if (upload.status == UPLOAD_FILE_START) {
             Serial.printf("Update: %s\n", upload.filename.c_str());
             if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-            Update.printError(Serial);
+                Update.printError(Serial);
             }
         } else if (upload.status == UPLOAD_FILE_WRITE) {
             /* flashing firmware to ESP*/
             if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-            Update.printError(Serial);
+                Update.printError(Serial);
             }
         } else if (upload.status == UPLOAD_FILE_END) {
             if (Update.end(true)) { //true to set the size to the current progress
-            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+                Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
             } else {
-            Update.printError(Serial);
+                Update.printError(Serial);
             }
         }
     });
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
 
-#include <WiFi.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266WebServer.h>
+  #include <ESP8266mDNS.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <WebServer.h>
+  #include <ESPmDNS.h>
+#endif
+
+// #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
+
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
 #include <RemoteDebug.h>
+
 #include <PubSubClient.h>
 #include <LittleFS.h>
 #include <FS.h>
@@ -16,68 +27,12 @@
 
 #define NUM(a) (sizeof(a) / sizeof(*a))
 
-
-  //See file .../hardware/espressif/esp32/variants/(esp32|doitESP32devkitV1)/pins_arduino.h
-  #define LED_BUILTIN       2         // Pin D2 mapped to pin GPIO2/ADC12 of ESP32, control on-board LED
-  
-  #define PIN_D0            0         // Pin D0 mapped to pin GPIO0/BOOT/ADC11/TOUCH1 of ESP32
-  #define PIN_D1            1         // Pin D1 mapped to pin GPIO1/TX0 of ESP32
-  #define PIN_D2            2         // Pin D2 mapped to pin GPIO2/ADC12/TOUCH2 of ESP32
-  #define PIN_D3            3         // Pin D3 mapped to pin GPIO3/RX0 of ESP32
-  #define PIN_D4            4         // Pin D4 mapped to pin GPIO4/ADC10/TOUCH0 of ESP32
-  #define PIN_D5            5         // Pin D5 mapped to pin GPIO5/SPISS/VSPI_SS of ESP32
-  #define PIN_D6            6         // Pin D6 mapped to pin GPIO6/FLASH_SCK of ESP32
-  #define PIN_D7            7         // Pin D7 mapped to pin GPIO7/FLASH_D0 of ESP32
-  #define PIN_D8            8         // Pin D8 mapped to pin GPIO8/FLASH_D1 of ESP32
-  #define PIN_D9            9         // Pin D9 mapped to pin GPIO9/FLASH_D2 of ESP32
-  
-  #define PIN_D10           10        // Pin D10 mapped to pin GPIO10/FLASH_D3 of ESP32
-  #define PIN_D11           11        // Pin D11 mapped to pin GPIO11/FLASH_CMD of ESP32
-  #define PIN_D12           12        // Pin D12 mapped to pin GPIO12/HSPI_MISO/ADC15/TOUCH5/TDI of ESP32
-  #define PIN_D13           13        // Pin D13 mapped to pin GPIO13/HSPI_MOSI/ADC14/TOUCH4/TCK of ESP32
-  #define PIN_D14           14        // Pin D14 mapped to pin GPIO14/HSPI_SCK/ADC16/TOUCH6/TMS of ESP32
-  #define PIN_D15           15        // Pin D15 mapped to pin GPIO15/HSPI_SS/ADC13/TOUCH3/TDO of ESP32
-  #define PIN_D16           16        // Pin D16 mapped to pin GPIO16/TX2 of ESP32
-  #define PIN_D17           17        // Pin D17 mapped to pin GPIO17/RX2 of ESP32     
-  #define PIN_D18           18        // Pin D18 mapped to pin GPIO18/VSPI_SCK of ESP32
-  #define PIN_D19           19        // Pin D19 mapped to pin GPIO19/VSPI_MISO of ESP32
-  
-  #define PIN_D21           21        // Pin D21 mapped to pin GPIO21/SDA of ESP32
-  #define PIN_D22           22        // Pin D22 mapped to pin GPIO22/SCL of ESP32
-  #define PIN_D23           23        // Pin D23 mapped to pin GPIO23/VSPI_MOSI of ESP32
-  #define PIN_D24           24        // Pin D24 mapped to pin GPIO24 of ESP32
-  #define PIN_D25           25        // Pin D25 mapped to pin GPIO25/ADC18/DAC1 of ESP32
-  #define PIN_D26           26        // Pin D26 mapped to pin GPIO26/ADC19/DAC2 of ESP32
-  #define PIN_D27           27        // Pin D27 mapped to pin GPIO27/ADC17/TOUCH7 of ESP32     
-  
-  #define PIN_D32           32        // Pin D32 mapped to pin GPIO32/ADC4/TOUCH9 of ESP32
-  #define PIN_D33           33        // Pin D33 mapped to pin GPIO33/ADC5/TOUCH8 of ESP32
-  #define PIN_D34           34        // Pin D34 mapped to pin GPIO34/ADC6 of ESP32
-  
-  //Only GPIO pin < 34 can be used as output. Pins >= 34 can be only inputs
-  //See .../cores/esp32/esp32-hal-gpio.h/c
-  //#define digitalPinIsValid(pin)          ((pin) < 40 && esp32_gpioMux[(pin)].reg)
-  //#define digitalPinCanOutput(pin)        ((pin) < 34 && esp32_gpioMux[(pin)].reg)
-  //#define digitalPinToRtcPin(pin)         (((pin) < 40)?esp32_gpioMux[(pin)].rtc:-1)
-  //#define digitalPinToAnalogChannel(pin)  (((pin) < 40)?esp32_gpioMux[(pin)].adc:-1)
-  //#define digitalPinToTouchChannel(pin)   (((pin) < 40)?esp32_gpioMux[(pin)].touch:-1)
-  //#define digitalPinToDacChannel(pin)     (((pin) == 25)?0:((pin) == 26)?1:-1)
-  
-  #define PIN_D35           35        // Pin D35 mapped to pin GPIO35/ADC7 of ESP32
-  #define PIN_D36           36        // Pin D36 mapped to pin GPIO36/ADC0/SVP of ESP32
-  #define PIN_D39           39        // Pin D39 mapped to pin GPIO39/ADC3/SVN of ESP32
-  
-  #define PIN_RX0            3        // Pin RX0 mapped to pin GPIO3/RX0 of ESP32
-  #define PIN_TX0            1        // Pin TX0 mapped to pin GPIO1/TX0 of ESP32
-  
-  #define PIN_SCL           22        // Pin SCL mapped to pin GPIO22/SCL of ESP32
-  #define PIN_SDA           21        // Pin SDA mapped to pin GPIO21/SDA of ESP32   
+#define LED_BUILTIN       2         // Pin D2 mapped to pin GPIO2/ADC12 of ESP32, control on-board LED  
+#define PIN_D0            0         // Pin D0 mapped to pin GPIO0/BOOT/ADC11/TOUCH1 of ESP32
+#define PIN_D25           25        // Pin D25 mapped to pin GPIO25/ADC18/DAC1 of ESP32
 
 const int TRIGGER_PIN = PIN_D0;   // Pin D0 mapped to pin GPIO0/BOOT/ADC11/TOUCH1 of ESP32
 const int TRIGGER_PIN2 = PIN_D25; // Pin D25 mapped to pin GPIO25/ADC18/DAC1 of ESP32
-
-#define LED_BUILTIN       2 // Pin D2 mapped to pin GPIO2/ADC12 of ESP32, control on-board LED
-
 
 RemoteDebug Debug;
 Blinker led(LED_BUILTIN);
@@ -96,9 +51,7 @@ class MQTT {
     String baseTopic;
 };
 
-
 MQTT mqtt;
-
 
 // End MQTT
 
@@ -131,9 +84,7 @@ void checkButton(){
       wm.startConfigPortal();
       debugI("Exiting Portal");
 
-
       if (saveConfig) {
-
         mqtt.server= String(custom_mqtt_server.getValue());
         mqtt.port= String(custom_mqtt_port.getValue());
 
