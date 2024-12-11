723c89fc9b4e02515fc4581d3587c313d2a54f4d
Jonathan Giles <jonathan@jonathangiles.net>
2024-12-12 11:40:44 +1300

    bunch of web ui improvements, but still a work in progress

--- data/www/espa.js
+++ data/www/espa.js
@@ -1,54 +1,92 @@
+/************************************************************************************************
+ * 
+ * Utility Methods
+ * 
+ ***********************************************************************************************/
 function confirmAction(url) {
     if (confirm('Are you sure?')) {
-    window.location.href = url;
+        window.location.href = url;
     }
 }
 
-function fetchStatus() {
-  fetch('/json')
-    .then(response => response.json())
-    .then(value_json => {
-      document.getElementById('temperatures_water').innerText = value_json.temperatures.water + "\u00B0C";
-      document.getElementById('temperatures_setPoint').value = value_json.temperatures.setPoint;
-      document.getElementById('status_state').innerText = value_json.status.state;
-      document.getElementById('status_controller').innerText = value_json.status.controller;
-      document.getElementById('status_firmware').innerText = value_json.status.firmware;
-      document.getElementById('status_serial').innerText = value_json.status.serial;
-      document.getElementById('status_siInitialised').innerText = value_json.status.siInitialised;
-      document.getElementById('status_mqtt').innerText = value_json.status.mqtt;
-      document.getElementById('espa_model').innerText = value_json.eSpa.model;
-      document.getElementById('espa_build').innerText = value_json.eSpa.update.installed_version;
-    })
-    .catch(error => console.error('Error fetching status:', error));
+function parseVersion(version) {
+    const match = version.match(/^v([\d.]+)/);
+    if (!match) return null;
+    return match[1].split('.').map(Number);
 }
 
+function compareVersions(current, latest) {
+    if (!current) return -1;
+    if (!latest) return 1;
+    for (let i = 0; i < Math.max(current.length, latest.length); i++) {
+        const a = current[i] || 0;
+        const b = latest[i] || 0;
+        if (a < b) return -1;
+        if (a > b) return 1;
+    }
+    return 0;
+}
+
+
+/************************************************************************************************
+ * 
+ * Status 
+ * 
+ ***********************************************************************************************/
+
+function fetchStatus() {
+    fetch('/json')
+        .then(response => response.json())
+        .then(value_json => {
+            document.getElementById('temperatures_water').innerText = value_json.temperatures.water + "\u00B0C";
+            document.getElementById('temperatures_setPoint').value = value_json.temperatures.setPoint;
+            document.getElementById('status_state').innerText = value_json.status.state;
+            document.getElementById('status_controller').innerText = value_json.status.controller;
+            document.getElementById('status_firmware').innerText = value_json.status.firmware;
+            document.getElementById('status_serial').innerText = value_json.status.serial;
+            document.getElementById('status_siInitialised').innerText = value_json.status.siInitialised;
+            document.getElementById('status_mqtt').innerText = value_json.status.mqtt;
+            document.getElementById('espa_model').innerText = value_json.eSpa.model;
+            document.getElementById('espa_build').innerText = value_json.eSpa.update.installed_version;
+        })
+        .catch(error => console.error('Error fetching status:', error));
+}
+
+window.onload = function () {
+    fetchStatus();
+    loadFotaData();
+    setInterval(fetchStatus, 10000);
+}
+
+
+/************************************************************************************************
+ * 
+ * Updating eSpa configuration
+ * 
+ ***********************************************************************************************/
+
 function updateTempSetPoint() {
-  const temperatures_setPoint = document.getElementById('temperatures_setPoint').value;
-  fetch('/set', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: 'temperatures_setPoint=' + temperatures_setPoint
-  })
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.error('Error setting temperature:', error));
+    const temperatures_setPoint = document.getElementById('temperatures_setPoint').value;
+    fetch('/set', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: 'temperatures_setPoint=' + temperatures_setPoint
+    })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.error('Error setting temperature:', error));
 }
 
 function sendCurrentTime() {
-  const status_datetime = new Date(Date() + " UTC").toISOString().slice(0, 19).replace("T", " ");
-  fetch('/set', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: 'status_datetime=' + status_datetime
-  })
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.error('Error setting temperature:', error));
-}
-
-window.onload = function() {
-    fetchStatus();
-    setInterval(fetchStatus, 10000);
+    const status_datetime = new Date(Date() + " UTC").toISOString().slice(0, 19).replace("T", " ");
+    fetch('/set', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: 'status_datetime=' + status_datetime
+    })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.error('Error setting datetime:', error));
 }
 
 // Retrieving and updating the configured settings, so they can be displayed in the modal popup
@@ -66,64 +104,273 @@ function loadConfig() {
         })
         .catch(error => {
             console.error('Error loading config:', error);
-            $('#configErrorAlert').show(); // Show the alert if there's an error
-    });
+            $('#configErrorAlert').text('Error loading configuration. Please try again.').show();
+        });
 }
 
-// Modal dialog content population
-$(document).ready(function() {
-    // JSON dump modal
-    $('#jsonLink').click(function(event) {
-        event.preventDefault();
-        fetch('/json').then(response => response.json()).then(data => {
-            $('#infoModalTitle').html("Spa JSON");
-            $('#infoModalBody').html('<pre>' + JSON.stringify(data, null, 2) + '</pre>');
-            $('#infoModal').modal('show');
-        }).catch(error => console.error('Error fetching JSON:', error));
-    });
-
-    // spa status modal
-    $('#statusLink').click(function(event) {
-        event.preventDefault();
-        fetch('/status').then(response => response.json()).then(data => {
-            $('#infoModalTitle').html("Spa Status");
-            $('#infoModalBody').html('<pre>' + data + '</pre>');
-            $('#infoModal').modal('show');
-        }).catch(error => console.error('Error fetching status:', error));
-    });
-
+// Configuration modal 
+$(document).ready(function () {
     // configuration settings modal
-    $('#configLink').click(function(event) {
+    $('#configLink').click(function (event) {
         event.preventDefault();
         $('#configModal').modal('show');
     });
+
     // Load configuration when the config modal is shown
-    $('#configModal').on('shown.bs.modal', function() {
+    $('#configModal').on('shown.bs.modal', function () {
         loadConfig();
     });
+
     // Handle form submission when the save button is clicked
-    $('#saveConfigButton').click(function() {
+    $('#saveConfigButton').click(function () {
         submitConfigForm();
     });
+
     function submitConfigForm() {
         $.ajax({
             url: '/config',
             type: 'POST',
             data: $('#config_form').serialize(),
-            success: function() {
-                // $('#msg').html('<p style="color:green;">Configuration updated successfully!</p>');
+            success: function () {
+                $('#msg').html('<p style="color:green;">Configuration updated successfully!</p>');
                 loadConfig();
-                setTimeout(function() { $('#msg').html(''); }, 3000);
+                setTimeout(function () { $('#msg').html(''); }, 3000);
                 $('#configModal').modal('hide');
             },
-            error: function() {
+            error: function () {
                 $('#configErrorAlert').text('Error updating configuration. Please try again.').show();
             }
         });
     }
 
-    $('#config_form').submit(function(e) {
+    $('#config_form').submit(function (e) {
         e.preventDefault();
         submitConfigForm();
     });
 });
+
+
+/************************************************************************************************
+ * 
+ * OTA Update Support
+ * 
+ ***********************************************************************************************/
+
+$(document).ready(function () {
+    // // FOTA modal
+    // $('#fotaLink').click(function (event) {
+    //     event.preventDefault();
+    //     $('#fotaModal').modal('show');
+    // });
+    // Delegate event listener for dynamically added #fotaLink
+    $(document).on('click', '#fotaLink', function (event) {
+        event.preventDefault();
+        $('#fotaModal').modal('show');
+        // loadFotaData();
+    });
+
+    // Enable the local install button when a file is selected
+    $('#localUpdateFileChooser').change(function () {
+        if ($(this).val()) {
+            $('#localInstallButton').prop('disabled', false);
+        } else {
+            $('#localInstallButton').prop('disabled', true);
+        }
+    });
+
+    // Handle local install button click
+    $('#localInstallButton').click(function () {
+        $('#upload_form').submit();
+    });
+
+    // Handle FOTA form submission for local update
+    $('#upload_form').submit(function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        $.ajax({
+            url: '/fota',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            xhr: function () {
+                var xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = evt.loaded / evt.total;
+                        percentComplete = parseInt(percentComplete * 100);
+                        $('#progressBar').css('width', percentComplete + '%').attr('aria-valuenow', percentComplete).text(percentComplete + '%');
+                    }
+                }, false);
+                return xhr;
+            },
+            success: function (data) {
+                $('#msg').html('<p style="color:green;">Firmware updated successfully!</p>');
+                setTimeout(function () { $('#msg').html(''); }, 3000);
+                $('#fotaModal').modal('hide');
+            },
+            error: function () {
+                $('#msg').html('<p style="color:red;">Error updating firmware. Please try again.</p>');
+            }
+        });
+    });
+
+    // Handle remote update installation
+    $('#remoteInstallButton').click(function (event) {
+        event.preventDefault();
+        var selectedVersion = $('#firmware-select').val();
+        if (selectedVersion) {
+            $.ajax({
+                url: '/install',
+                type: 'POST',
+                data: { version: selectedVersion },
+                success: function (data) {
+                    $('#msg').html('<p style="color:green;">Firmware installed successfully!</p>');
+                    setTimeout(function () { $('#msg').html(''); }, 3000);
+                    $('#fotaModal').modal('hide');
+                },
+                error: function () {
+                    $('#msg').html('<p style="color:red;">Error installing firmware. Please try again.</p>');
+                }
+            });
+        }
+    });
+
+    // Show/hide update sections based on selected update method
+    $('#updateMethod').change(function () {
+        var selectedMethod = $(this).val();
+        if (selectedMethod === 'remote') {
+            $('#remoteUpdate').show();
+            $('#localUpdate').hide();
+        } else if (selectedMethod === 'local') {
+            $('#remoteUpdate').hide();
+            $('#localUpdate').show();
+        } else {
+            $('#remoteUpdate').hide();
+            $('#localUpdate').hide();
+        }
+    });
+});
+
+function loadFotaData() {
+    fetch('/json')
+        .then(response => response.json())
+        .then(value_json => {
+            document.getElementById('espa_model').innerText = value_json.eSpa.model;
+            document.getElementById('installedVersion').innerText = value_json.eSpa.update.installed_version;
+        })
+        .catch(error => console.error('Error fetching FOTA data:', error));
+
+    $.ajax({
+        url: 'https://api.github.com/repos/wayne-love/ESPySpa/releases',
+        type: 'GET',
+        success: function (data) {
+            document.getElementById('lastestRelease').innerText = data[0].tag_name;
+
+            // Populate the select dropdown with all releases
+            const firmwareSelect = document.getElementById('firmware-select');
+            firmwareSelect.innerHTML = ''; // Clear existing options
+
+            // Add default disabled option
+            const defaultOption = document.createElement('option');
+            defaultOption.value = '';
+            defaultOption.text = 'Select a version';
+            defaultOption.disabled = true;
+            defaultOption.selected = true;
+            firmwareSelect.appendChild(defaultOption);
+
+            // Add release options
+            data.forEach(release => {
+                const option = document.createElement('option');
+                option.value = release.tag_name;
+                option.text = release.tag_name;
+                firmwareSelect.appendChild(option);
+            });
+
+            // Enable the install button when a valid choice is selected
+            firmwareSelect.addEventListener('change', function () {
+                if (firmwareSelect.value) {
+                    $('#remoteInstallButton').prop('disabled', false);
+                } else {
+                    $('#remoteInstallButton').prop('disabled', true);
+                }
+            });
+
+            // Check for new version
+            const latestVersion = parseVersion(data[0].tag_name);
+            const currentVersion = parseVersion(document.getElementById('installedVersion').innerText);
+            const comparison = compareVersions(currentVersion, latestVersion);
+            if (comparison < 0) {
+                showAlert(`There is a new eSpa release available - it's version ${data[0].tag_name}. You can <a href="#" id="fotaLink">update now</a>.`, 'alert-primary', "New eSpa release!");
+            }
+        },
+        error: function () {
+            showAlert('Failed to fetch eSpa release information. If this persists, take a look at our <a href="https://espa.diy/troubleshooting.html">troubleshooting docs</a>.', 'alert-danger', "Error");
+        }
+    });
+}
+
+
+/************************************************************************************************
+ * 
+ * Status models (for JSON and Spa response)
+ * 
+ ***********************************************************************************************/
+
+$(document).ready(function () {
+    // JSON dump modal
+    $('#jsonLink').click(function (event) {
+        event.preventDefault();
+        fetch('/json').then(response => response.json()).then(data => {
+            $('#infoModalTitle').html("Spa JSON");
+            $('#infoModalBody').html('<pre>' + JSON.stringify(data, null, 2) + '</pre>');
+            $('#infoModal').modal('show');
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+            showAlert('Error connecting to the spa.  If this persists, take a look at our <a href="https://espa.diy/troubleshooting.html">troubleshooting docs</a>.', 'alert-danger', "Error");
+        });
+    });
+
+    // spa status modal
+    $('#statusLink').click(function (event) {
+        event.preventDefault();
+        fetch('/status').then(response => response.text()).then(data => {
+            $('#infoModalTitle').html("Spa Status");
+            $('#infoModalBody').html('<pre>' + data + '</pre>');
+            $('#infoModal').modal('show');
+        })
+        .catch(error => {
+            console.error('Error fetching status:', error);
+            showAlert('Error connecting to the spa.  If this persists, take a look at our <a href="https://espa.diy/troubleshooting.html">troubleshooting docs</a>.', 'alert-danger', "Error");
+        });
+    });
+});
+
+
+/************************************************************************************************
+ * 
+ * Front page alerts
+ * 
+ ***********************************************************************************************/
+
+function showAlert(message, alertClass, title = '') {
+    const pageAlert = $('#page-alert');
+    const pageAlertParent = $('.page-alert-parent');
+
+    // Clear existing alert classes and set the new class
+    pageAlert.removeClass(function (index, className) {
+        return (className.match(/(^|\s)alert-\S+/g) || []).join(' ');
+    }).addClass(alertClass);
+
+    // Construct the alert content
+    let alertContent = '';
+    if (title) {
+        alertContent += `<h4 class="alert-heading">${title}</h4>`;
+    }
+    alertContent += message;
+
+    // Set the alert content and show the alert
+    pageAlert.html(alertContent);
+    pageAlertParent.show();
+}
